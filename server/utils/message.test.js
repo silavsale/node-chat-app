87de9b7e778be9fc3af93083a9d7d24869c2165b
Silavsale <silavsale@gmail.com>
2019-01-30 14:22:46 -0500

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,16 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'phil';
+        var text = 'some message';
+        var message = generateMessage(from, text);
+
+        expect(typeof message.createdAt).toBe('number');
+        expect(message).toMatchObject({from, text});
+
+    });
+});
+
